d0c31d3542f97ee1684c3ba63dcd468c6ce66552
David Chiang <dchiang@nd.edu>
2020-12-17 21:38:26 -0500

    section numbers in html

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pandoc --toc-depth 1 --toc main.tex --mathjax -f latex -H start.tmp -t html -s -o index.html --bibliography references.bib
+pandoc --toc-depth 1 --toc --number-section main.tex --mathjax -f latex -H start.tmp -t html -s -o index.html --bibliography references.bib
